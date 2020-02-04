00f2a4dbea2b64653c50d65c339d6e292ddc420c
ittisafur <ittisafur@gmail.com>
2020-02-04 20:42:00 +0600

    Orders

---
+++ database/migrations/2020_02_03_150158_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('country')->nullable();
+            $table->string('street_address')->nullable();
+            $table->string('city');
+            $table->integer('zip');
+            $table->string('cell');
+            $table->string('email');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('orders');
+    }
+}
